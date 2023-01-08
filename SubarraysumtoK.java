f029203718cfcdbd4b0022344a16186a5f837a93
Suraj <CkSuraj@gmail.com>
2023-01-08 14:53:42 +0530

    subarry sum equal to k java DSA ?

---
+++ SubarraysumtoK.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class SubarraysumtoK {
+    
+    public static void main(String[] args) {
+        int nums[]={1,3,4,6,7};
+        int k=4;
+        HashMap<Integer ,Integer> map= new HashMap<>();
+        map.put(0,1);
+        int sum=0;
+        int ans=0;
+         for(int i=0;i<nums.length;i++){
+                   sum+=nums[i];
+                   if(map.containsKey(sum-k)){
+                       ans+=map.get(sum-k);
+                   } 
+                   map.put(sum,map.getOrDefault(sum,0)+1);
+         }
+        System.out.println(ans);
+ }
+    }
+
